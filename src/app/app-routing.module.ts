42e9f4953bbbd34a4206d91da2d92a62ef537d4e
yassine7h <110841721+yassine7h@users.noreply.github.com>
2024-03-13 23:19:27 +0100

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductsComponent} from "./products/products.component";
+import {AddProductComponent} from "./add-product/add-product.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products',  component: ProductsComponent },
+  { path: 'products/add',  component: AddProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductsComponent } from './products/products.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { HttpClientModule} from "@angular/common/http";
+import { PricePipe } from './custom-pipes/price.pipe';
+import { DetailsProductComponent } from './details-product/details-product.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProductsComponent,
+    AddProductComponent,
+    PricePipe,
+    DetailsProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
